f829e0d64bb3dccab573e95827451fd666a77efe
Mohamad Dahhan <72216148+Terminal-Grasshopper@users.noreply.github.com>
2022-07-04 19:02:43 +0000

    bcrypt done

--- server.js
+++ server.js
@@ -7,17 +7,29 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
+const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  /*Store hash in your db*/
+  console.log(hash) //$2b$12$Vm8yHu5PoJyMszOJCSwEj.YnezpuicQtll4wzv.jop4NRMYkbBpai
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+  /*res == true or false*/
+  console.log(res)//true
+});
+});
 
 
 //END_ASYNC
 
 //START_SYNC
 
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
